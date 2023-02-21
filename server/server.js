e5206a2cf51dbdb06d7a2ba8285bbc18ac154c42
= <mukherjee4004@gmail.com>
2023-02-21 10:24:11 +0530

    UseReducer , server end point for getting items

--- server/server.js
+++ server/server.js
@@ -15,7 +15,24 @@ app.get("/api/products" , (req , res)=>{
     
     res.send(data.products)
 })
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 
+// getting items with the id
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
